ad8cfae1ff6d18796a2d53a79546131319271f47
Chummy240 <30804732+Chummy240@users.noreply.github.com>
2017-08-10 15:39:29 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,17 +81,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get ('/articleone', function (req, res) {
-  res.send(createTemplate(articleone))
+app.get ('/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]))
 });
 
-app.get ('/articlethree', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'articlethree.html'));
-});
-
-app.get ('/articletwo', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
