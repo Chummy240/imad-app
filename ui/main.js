57cbfb11e79a2deafb4f6aa79eba0afb7c4a224b
Chummy240 <30804732+Chummy240@users.noreply.github.com>
2017-08-21 16:52:25 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,12 @@
-console.log('Loaded!');
-
-var img = document.getElementById('maid');
-
-img.onclick = function () { 
-    var marginLeft = 0;
-    function moveRight () {
-        marginLeft = marginLeft = 1;
-        img.style.marginLeft = marginLeft + 'px';
-    }
-    var interval = setInterval(moveRight, 50);
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function () {
+ 
+ 
+ 
+ counter = counter + 1;
+ var span = document.getElementById('count');
+ span.innerHTML = counter.toString();
+ 
     
 };
