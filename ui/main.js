deb68cd86edb9ae1d407883ba8104e3799851331
Chummy240 <30804732+Chummy240@users.noreply.github.com>
2017-08-21 18:52:39 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,11 +30,10 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    //request server, fetch name 
-    // make names a list
+    
     var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
-    for (var i=0; i<names.length; i++) {
+    for (var i=0; i< names.length; i++) {
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElemenetById('namelist');
