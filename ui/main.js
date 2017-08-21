1438429c188a48d2fde39389304fdcb30ce76d0f
Chummy240 <30804732+Chummy240@users.noreply.github.com>
2017-08-21 16:54:41 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 var button = document.getElementById('counter');
 var counter = 0;
+
 button.onclick = function () {
  
  
@@ -7,6 +8,4 @@ button.onclick = function () {
  counter = counter + 1;
  var span = document.getElementById('count');
  span.innerHTML = counter.toString();
- 
-    
 };
