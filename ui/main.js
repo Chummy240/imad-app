c5f9cdbde59a661378839d2ced78524ca7f9d645
Chummy240 <30804732+Chummy240@users.noreply.github.com>
2017-08-25 16:47:35 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,4 @@
-var button = document.getElementById('counter');
 
-
-button.onclick = function () {
-    //create request
-    var request = new XMLHttpRequest();
- 
-    //catch response and store in variable
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            //action
-            if (request.status === 200) {
-           var counter = request.responseText;
-           var span = document.getElementById('count');
-           span.innerHTML = counter.toString();
-           }
-        }
-   //not done
-    };
-    
-    
-    //make request
-    request.open('GET', 'http://ninavrajan.imad.hasura-app.io/counter', true);
-    request.send(null);
-    
- 
-};
 //submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
@@ -39,25 +13,26 @@ submit.onclick = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             //action
             if (request.status === 200) {
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for (var i=0; i< names.length; i++) {
-                    list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
-           }
+                console.log('user logged in');
+                alert('Login successful :D');
+            } else if (request.status === 403) {
+                alert('Username/passowrd is incorrect :(');
+            } else if (request.status === 500) {
+                alert('Something has gone wrong...');
+            }
         }
    //not done
     };
     
     
     //make request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', 'http://ninavrajan.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://ninavrajan.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
     
    
 };
